285cb2c216b658029b795b175571116c24ee1cca
Violet <violetbutagain@gmail.com>
2023-09-22 03:05:38 +0000

    awesome

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 const express = require('express'),
-// ws = require('ws'),
 path = require('path'),
 fs = require('fs')
-
-// const websocket = new ws.WebSocket('wss://api.lanyard.rest/socket', {})
-
-// var heartInt = 30000
-
-// function heartbeat() {
-//     websocket.send(JSON.stringify({op: 3}))
-
-//     setTimeout(() => {
-//         heartbeat()
-//     }, heartInt);
-// }
-
-// websocket.on('message', function mess(buffer, bing) {
-//     var data = JSON.parse(buffer)
-//     var opc = data.op
-
-//     console.log(data)
-    
-//     if (opc == 1) {
-//         heartint = buffer.heartbeat_interval
-//         heartbeat()
-//     }
-// })
 
 var app = express()
 
