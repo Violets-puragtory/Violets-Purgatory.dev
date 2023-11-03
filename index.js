36d82d9c790d779f75325849e3657659521b767b
Violet <violetbutagain@gmail.com>
2023-11-03 18:01:13 +0000

    Curl is funni

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const staticpath = path.join(__dirname, (process.env.STATIC_PATH || 'static/'))
 
 app.use(express.static(staticpath))
 
+app.get("/curl", (req, res) => {
+    res.sendFile(path.join(__dirname, "resources/curl"))
+})
+
 app.listen(PORT, () => {
     console.log("Violets-Purgatory is now listening on port: " + PORT)
 })
