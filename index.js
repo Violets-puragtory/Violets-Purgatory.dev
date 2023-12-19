5b0c5eef086b05b3a6feba74f58c46a020131761
bingus_violet <violet@violets-purgatory.dev>
2023-12-19 18:51:41 +0000

    asdasdasd

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ function pageUpdate() {
         html = html.replace("{SEASONAL_EFFECT}", "")
     }
 
-    html = html.replace("{COMMITS}", commitCount('https://github.com/Violets-puragtory/Violets-Purgatory.dev'))
+    html = html.replace("{COMMITS}", commitCount('https://codeberg.org/Bingus_Violet/Violets-Purgatory.git'))
 
     fs.writeFileSync(path.join(__dirname, 'static/index.html'), html)
 }
