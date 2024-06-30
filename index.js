859997c48e21bbe63b99058792404863feaf803b
bingus_violet <violet@violets-purgatory.dev>
2024-06-30 02:29:09 -0500

    fix error causing error

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const fs = require("fs")
+
 require("./fileHandler.js")
 require('./expressHandler.js')
+require("./imageEmbedder.js")
 
 process.on('uncaughtException', (err, origin) => {
     fs.writeSync(
